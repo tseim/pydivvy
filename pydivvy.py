e66b36206022d30ccef8e3fdfa4cfeb196e0b1c9
Tim Seim <tim.seim@googlemail.com>
2010-07-12 18:09:41 +0200

    Code cleanup

--- pydivvy.py
+++ pydivvy.py
@@ -1,25 +1,5 @@
 from Tkinter import *
-import re
 import commands
-import subprocess
-
-
-def getWorkArea():
-    """ Return workarea geometry of current desktop
-        (this means the area without panels)
-
-        Use 2-tuple (x, y)
-    """
-    for line in commands.getoutput("wmctrl -d").split("\n"):
-        if '*' in line: # '*' marks current desktop
-            break
-    geometry = line.split(None, 9)[8]
-    return tuple(map(int, geometry.split('x')))
-
-def kill_wmctrl():
-    cmd = "killall wmctrl"
-    o = commands.getoutput(cmd)
-    print o
 
 def placeWindow(x,y,w,h):
     cmd = "wmctrl -r :SELECT: -e 0,{x:d},{y:d},{w:d},{h:d}".format(x=x,y=y,w=w,h=h)
@@ -41,37 +21,29 @@ def splitCeil(seq, m):
     return newseq
 
 
-def getBlockBounds(val, end, num_blocks):
-    seq=range(end)
-    n,b,newseq=len(seq),0,[]
-    for k in range(num_blocks):
-        q,r=divmod(n-k,num_blocks)
-        a,b=b,b+q+(r!=0)
-        if val in seq[a:b]:
-            return seq[a], seq[b-1]
-
-def parseGeometry(geometry):
-    m = re.match("(\d+)x(\d+)([-+]\d+)([-+]\d+)", geometry)
-    if not m:
-        raise ValueError("failed to parse geometry string")
-    return map(int, m.groups())
-
 
 class App(Frame):
     def __init__(self, master=None, width=300, height=200, rows=6, cols=10):
         Frame.__init__(self, master)
 
-        self.lastcoords = []
         self.rows = rows
         self.cols = cols
         self.width = width
         self.height = height
-        self.lastcell= ()
+        self.lastcol= 0
+        self.lastrow = 0
+        self.screenwidth = self.master.winfo_screenwidth()
+        self.screenheight = self.master.winfo_screenheight()
+        self.screen_width_ranges = splitCeil(range(self.screenwidth), cols)
+        self.screen_height_ranges = splitCeil(range(self.screenheight), rows)
+        self.window_width_ranges = splitCeil(range(width), cols)
+        self.window_height_ranges = splitCeil(range(height), rows)
 
-        self.x1 = self.y1 = self.x2 = self.y2 = 0
-        
+        self.gridColor = "DarkGrey"
+        self.selectionColor = "red"
+
         if (master is not None):
-            self.master.title("Selector")
+            self.master.title("pydivvy")
             self.master.resizable(False,False)
             self.master.maxsize(self.width, self.height)
             self.master.minsize(self.width, self.height)
@@ -80,22 +52,20 @@ class App(Frame):
         self.createWidgets()
         self.drawGrid();
         self.centerWindow()
-        #self.master.overrideredirect(False)
+       
 
     def drawGrid(self):
-        ranges = splitCeil(range(self.width), self.cols)
-        for r in ranges:
-            self.canvas.create_line(r[0],0,r[0],self.height, fill="DarkGrey")
+        for r in self.window_width_ranges:
+            self.canvas.create_line(r[0], 0, r[0], self.height, fill=self.gridColor)
 
-        ranges = splitCeil(range(self.height),self.rows)
-        for r in ranges:
-            self.canvas.create_line(0,r[0],self.width,r[0], fill="DarkGrey")
+        for r in self.window_height_ranges:
+            self.canvas.create_line(0, r[0], self.width, r[0], fill=self.gridColor)
 
 
 
     def centerWindow(self):
-        ws = self.master.winfo_screenwidth()
-        hs = self.master.winfo_screenheight()
+        ws = self.screenwidth
+        hs = self.screenheight
 
         w, h = self.width, self.height
         x = (ws/2) - (w/2) 
@@ -104,70 +74,29 @@ class App(Frame):
 
 
     def createWidgets(self):
-        self.master.wm_attributes("-alpha", 0.3)
-        self.canvas = Canvas(self, width=self.width, height=self.height)#, bg="black")
+        self.master.wm_attributes("-alpha", 0.3) #doesnt work.
+        self.canvas = Canvas(self, width=self.width, height=self.height)
         
         self.canvas.grid(column=0, row=0, sticky=(N,W,E,S))
         self.canvas.pack(side=LEFT)
-        self.canvas.bind("<Button-1>", self.updateXY)
+        self.canvas.bind("<Button-1>", self.setLastCell)
         self.canvas.bind("<B1-Motion>", self.drawRect)
         self.canvas.bind("<B1-ButtonRelease>", self.doneStroke)
-        self.canvas.bind("<Button-3>", self.rightClick)
 
-    
+    def getAreaDict(self, curr_x, curr_y, ranges_x, ranges_y):
+        current_col = [i for (i, rng) in enumerate(self.window_width_ranges) if curr_x in rng][0]
+        current_row = [i for (i, rng) in enumerate(self.window_height_ranges) if curr_y in rng][0]
 
-    def getCoords(self, x, y):
-        w,h,xoff,yoff = parseGeometry(self.master.geometry());
-        rx = getBlockBounds(x, w, self.cols)
-        ry = getBlockBounds(y, h, self.rows)
-        return rx,ry
-
-
-    #------------ callbacks-------------------
-    def updateXY(self, event):
-        lastx, lasty = self.canvas.canvasx(event.x), self.canvas.canvasy(event.y)
-        crds = self.getCoords(lastx,lasty)
-        self.lastcoords = crds
-
-        col = [i for (i, rng) in enumerate(splitCeil(range(self.width), self.cols)) if lastx in rng][0]
-        row = [i for (i, rng) in enumerate(splitCeil(range(self.height), self.rows)) if lasty in rng][0]
+        prev_col, prev_row = self.lastcol, self.lastrow
         
-        self.lastcell = (col, row)
-        #print self.lastcell
-
-    def drawRect(self, event):
-        x,y = self.canvas.canvasx(event.x), self.canvas.canvasy(event.y)
-        self.canvas.delete(ALL)
-        self.drawGrid()
-        newcoords= self.getCoords(x,y);
-        lastcoords= self.lastcoords
-        x1 = min(newcoords[0][0], newcoords[0][1], lastcoords[0][0], lastcoords[0][1])
-        y1 = min(newcoords[1][1], newcoords[1][0], lastcoords[1][1], lastcoords[1][0])
-        x2 = max(newcoords[0][0], newcoords[0][1], lastcoords[0][0], lastcoords[0][1])
-        y2 = max(newcoords[1][1], newcoords[1][0], lastcoords[1][1], lastcoords[1][0])
-
-        self.x1 = min(x1,x2)
-        self.y1 = min(y1,y2)
-        self.x2 = max(x2,x1)
-        self.y2 = max(y2,y1)
-        
-        self.canvas.create_rectangle((self.x1, self.y1, self.x2, self.y2), width=2, outline="red")
-
-    def doneStroke(self, event):
-        x,y = self.canvas.canvasx(event.x), self.canvas.canvasy(event.y)
-        current_col = [i for (i, rng) in enumerate(splitCeil(range(self.width), self.cols)) if x in rng][0]
-        current_row = [i for (i, rng) in enumerate(splitCeil(range(self.height), self.rows)) if y in rng][0]
-
-        prev_col, prev_row = self.lastcell[0], self.lastcell[1]
-        
-        disp_w_total, disp_h_total = getWorkArea()
-        ranges_w = splitCeil(range(disp_w_total), self.cols)
-        ranges_h = splitCeil(range(disp_h_total), self.rows)
+        ranges_w = ranges_x
+        ranges_h = ranges_y
         
         disp_x = min(ranges_w[prev_col][0], \
                      ranges_w[prev_col][-1], \
                      ranges_w[current_col][0], \
                      ranges_w[current_col][-1])
+
         disp_y = min(ranges_h[prev_row][0], \
                      ranges_h[prev_row][-1], \
                      ranges_h[current_row][0], \
@@ -177,30 +106,49 @@ class App(Frame):
                       ranges_w[prev_col][-1],\
                       ranges_w[current_col][0],\
                       ranges_w[current_col][-1])
+
         disp_y2 = max(ranges_h[prev_row][0],\
                       ranges_h[prev_row][-1],\
                       ranges_h[current_row][0],\
                       ranges_h[current_row][-1])
+
+        return dict(min_x=disp_x, max_x=disp_x2, min_y=disp_y, max_y=disp_y2)        
+
+    #------------ callbacks-------------------
+    def setLastCell(self, event):
+        lastx, lasty = self.canvas.canvasx(event.x), self.canvas.canvasy(event.y)
+
+        col = [i for (i, rng) in enumerate(self.window_width_ranges) if lastx in rng][0]
+        row = [i for (i, rng) in enumerate(self.window_height_ranges) if lasty in rng][0]
+        
+        self.lastcol, self.lastrow = col, row
+
+
+    def drawRect(self, event):
+        x,y = self.canvas.canvasx(event.x), self.canvas.canvasy(event.y)
+        self.canvas.delete(ALL)
+        self.drawGrid()
+
+        d = self.getAreaDict(x, y, self.window_width_ranges, self.window_height_ranges)
+        x1, x2, y1, y2 = d["min_x"], d["max_x"], d["min_y"], d["max_y"]
+
+        self.canvas.create_rectangle((x1, y1, x2, y2),\
+                                     width=2,\
+                                     outline=self.selectionColor)
+
+
+    def doneStroke(self, event):
+        x,y = self.canvas.canvasx(event.x), self.canvas.canvasy(event.y)
+
+        d = self.getAreaDict(x, y, self.screen_width_ranges, self.screen_height_ranges)
+        disp_x, disp_x2, disp_y, disp_y2 = d["min_x"], d["max_x"], d["min_y"], d["max_y"]
         
         width  = disp_x2 - disp_x
         height = disp_y2 - disp_y
-
-        print disp_x, disp_y, disp_x2, disp_y2, width, height
         
-        placeWindow(disp_x, \
-                    disp_y, \
-                    width, \
-                    height)
+        placeWindow(disp_x, disp_y, width, height)
 
-        
-
-    def rightClick(self,event):
-        kill_wmctrl()
-
-
-
+     
 root = Tk()
-#root.columnconfigure(0, weight=1)
-#root.rowconfigure(0, weight=1)
 myapp = App(master=root, width=800, height=600, rows=6, cols=6)
 myapp.mainloop()
